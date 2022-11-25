17c3f6ccb8e28610aaf40952435152fc2fec83ef
Chris Seaman <christopherseaman1983@gmail.com>
2022-11-25 13:12:41 +0000

    update

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,4 @@
-const REAL_URL = `https://vans-db-api.netlify.app/.netlify/functions/api/vans`;
-const DEV_URL = `http://localhost:9000/.netlify/functions/api/vans`;
+const REAL_URL = `https://vans-db-api.netlify.app/.netlify/functions/api`;
+const DEV_URL = `http://localhost:9000/.netlify/functions/api`;
 
 export const API_URL = REAL_URL;
